f42aa93af35130a60bb1fe45f48143127e7fd824
chenduowei <duowei_chen@outlook.com>
2023-04-07 01:22:27 +0800

    Initial Commit

---
+++ MovieRecommender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieRecommender
+//
+//  Created by chenduowei on 2023/4/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieRecommender/MovieRecommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecommenderApp.swift
+//  MovieRecommender
+//
+//  Created by chenduowei on 2023/4/7.
+//
+
+import SwiftUI
+
+@main
+struct MovieRecommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
